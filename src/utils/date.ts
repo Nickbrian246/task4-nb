941ffc95392ee3f1e2f836732759edb9a0277917
nick brian <nbjm1234@outlook.com>
2024-07-24 15:12:35 -0600

    fixing bugs

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,7 +4,21 @@ export function formatDate(dateRow: Date) {
   const minutes = date.getUTCMinutes().toString().padStart(2, "0");
   const seconds = date.getUTCSeconds().toString().padStart(2, "0");
   const day = date.getUTCDate().toString().padStart(2, "0");
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const shortMonthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = date.getUTCMonth();
   const year = date.getUTCFullYear();
-  return `${hours}:${minutes}:${seconds}, ${day},${month},${year}`;
+  return `${hours}:${minutes}:${seconds}, ${day} ${shortMonthNames[month]}, ${year}`;
 }
--- src/validations/users/index.ts
+++ src/validations/users/index.ts
@@ -1,5 +1,5 @@
 import { string, z } from "zod";
 
 export const groupOfUsersSchema = z.object({
-  users: z.array(z.string()),
+  users: z.array(z.string()).min(1),
 });
